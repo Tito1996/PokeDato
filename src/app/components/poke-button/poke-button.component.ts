f920ab66f0b3c7ca270d9ed541e9de9cbb7bef56
Vicente <vicente.ict3@gmail.com>
2025-03-09 12:56:34 +0100

    Angular added & Home component created

---
+++ src/app/components/poke-button/poke-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-button',
+  imports: [],
+  templateUrl: './poke-button.component.html',
+  styleUrl: './poke-button.component.css'
+})
+export class PokeButtonComponent {
+
+}
